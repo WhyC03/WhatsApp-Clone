66b59f5474f7e7109d0cb6f9fcd09cf1855afd1a
Yash Chandra <yash24.chandra2003@gmail.com>
2024-07-29 23:58:55 +0530

    First Design Complete

---
+++ lib/screens/mobile_chat_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/info.dart';
+import 'package:whatsapp_clone/widgets/chat_list.dart';
+
+class MobileChatScreen extends StatelessWidget {
+  const MobileChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: appBarColor,
+        title: Text(
+          info[0]['name'].toString(),
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.video_call),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.call),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ChatList(),
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/mobile_screen_layout.dart
+++ lib/screens/mobile_screen_layout.dart
@@ -1,13 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/widgets/contacts_list.dart';
 
 class MobileScreenLayout extends StatelessWidget {
   const MobileScreenLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('This is Mobile'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: appBarColor,
+          elevation: 0,
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(
+              color: Colors.grey,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert,
+                color: Colors.grey,
+              ),
+            ),
+          ],
+          bottom: const TabBar(
+            indicatorColor: tabColor,
+            indicatorWeight: 4,
+            labelColor: tabColor,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+            tabs: [
+              Text('CHATS'),
+              Text('STATUS'),
+              Text('CALLS'),
+            ],
+          ),
+        ),
+        body: const ContactsList(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: tabColor,
+          child: const Icon(
+            Icons.comment,
+            color: Colors.white,
+          ),
+        ),
       ),
     );
   }
